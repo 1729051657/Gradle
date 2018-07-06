df82b740649e1fc67236a7cc1818d70691ce4c61
Raytine <1729051657@qq.com>
2018-07-06 14:46:44 +0800

    1

---
+++ zcwl/src/main/java/com/zcwl/ZcwlApplication.java
@@ -0,0 +1,12 @@
+package com.zcwl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZcwlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZcwlApplication.class, args);
+	}
+}
